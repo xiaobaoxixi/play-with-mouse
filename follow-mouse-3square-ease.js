a4f143f79f820e1a9cc88922495506668435ea74
liyan <yanx0167@stud.kea.dk>
2018-09-07 18:20:40 +0200

    3 squares flee with ease effect finished. NO cancel on rAF yetgit add follow-mouse-3square-ease.js index.html

--- follow-mouse-3square-ease.js
+++ follow-mouse-3square-ease.js
@@ -6,14 +6,20 @@ const squareHeight = square.clientHeight;
 const squareWidth = square.clientWidth;
 const windowHeight = window.innerHeight;
 const windowWidth = window.innerWidth;
+let mousePositionX;
+let mousePositionY;
 let targetX;
 let targetY;
 let squarePositionX;
 let squarePositionY;
-const steps1 = 31;
-const steps2 = 23;
-const steps3 = 11;
-
+let squarePositionX1;
+let squarePositionY1;
+let squarePositionX2;
+let squarePositionY2;
+const steps = 31;
+const steps1 = 23;
+const steps2 = 11;
+let RAF;
 window.addEventListener("DOMContentLoaded", centerSquare);
 
 function centerSquare() {
@@ -27,25 +33,42 @@ function centerSquare() {
 }
 function checkMouse() {
   window.addEventListener("mousemove", getMouseMovement);
+  moveSquare();
 }
 function getMouseMovement(m) {
-  moveSquare(m.pageX, m.pageY);
+  mousePositionX = m.pageX;
+  mousePositionY = m.pageY;
 }
-function moveSquare(x, y) {
+function moveSquare() {
   squarePositionX = square.offsetLeft;
   squarePositionY = square.offsetTop;
+  squarePositionX1 = square1.offsetLeft;
+  squarePositionY1 = square1.offsetTop;
+  squarePositionX2 = square2.offsetLeft;
+  squarePositionY2 = square2.offsetTop;
 
   targetX =
     windowWidth / 2 -
     squareWidth / 2 +
-    (windowWidth / 2 - x) * ((windowWidth - squareWidth) / windowWidth);
+    (windowWidth / 2 - mousePositionX) *
+      ((windowWidth - squareWidth) / windowWidth);
   targetY =
     windowHeight / 2 -
     squareHeight / 2 +
-    (windowHeight / 2 - y) * ((windowHeight - squareHeight) / windowHeight);
+    (windowHeight / 2 - mousePositionY) *
+      ((windowHeight - squareHeight) / windowHeight);
 
   squarePositionX += (targetX - squarePositionX) / steps;
   squarePositionY += (targetY - squarePositionY) / steps;
+  squarePositionX1 += (targetX - squarePositionX1) / steps1;
+  squarePositionY1 += (targetY - squarePositionY1) / steps1;
+  squarePositionX2 += (targetX - squarePositionX2) / steps2;
+  squarePositionY2 += (targetY - squarePositionY2) / steps2;
   square.style.left = `${squarePositionX}px`;
   square.style.top = `${squarePositionY}px`;
+  square1.style.left = `${squarePositionX1}px`;
+  square1.style.top = `${squarePositionY1}px`;
+  square2.style.left = `${squarePositionX2}px`;
+  square2.style.top = `${squarePositionY2}px`;
+  RAF = requestAnimationFrame(moveSquare);
 }
