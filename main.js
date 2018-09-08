d82e322477531037eb079fe6114b941cb4d1753d
liyan <yanx0167@stud.kea.dk>
2018-09-08 23:28:05 +0200

    fixed: section height of empty iframe

--- main.js
+++ main.js
@@ -17,6 +17,7 @@ function checkIfDone(li, index) {
     liS[index].style.color = "lightgrey";
     liS[index].style.cursor = "none";
     iframe.style.height = "0";
+    iframe.parentElement.style.paddingTop = "0px";
   }
   // instead of using the commented out code to seperate the function (which files without click event), use the code below
   li.addEventListener("click", jumpToSection);
