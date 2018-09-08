fb1b8a024d0863950356adb220ce71559a84087d
liyan <yanx0167@stud.kea.dk>
2018-09-08 20:21:46 +0200

    zoom in/out keeping center finished.

--- drag-n-zoom3.js
+++ drag-n-zoom3.js
@@ -13,15 +13,23 @@ let dragging = false;
 const img = document.querySelector(".img");
 const zoomIn = document.querySelector(".zoom-in");
 const zoomOut = document.querySelector(".zoom-out");
-const center = document.querySelector(".center");
+const centerButton = document.querySelector(".center");
+const centerPositionX = window.innerWidth / 2;
+const centerPositionY = window.innerHeight / 2;
+const x = document.querySelector("aside h1");
 
 function init() {
+  centerX();
   window.addEventListener("mousedown", startDrag);
   window.addEventListener("mousemove", whileDrag);
   window.addEventListener("mouseup", stopDrag);
   zoomIn.addEventListener("click", zoomInStep);
   zoomOut.addEventListener("click", zoomOutStep);
-  center.addEventListener("click", reCenter);
+  centerButton.addEventListener("click", reCenter);
+}
+function centerX() {
+  x.style.left = `${(window.innerWidth - x.clientWidth) / 2}px`;
+  x.style.top = `${(window.innerHeight - x.clientHeight) / 2 - 15}px`; // -15 is not precise, just eyeballed. font size setting influence element height,
 }
 function startDrag(m) {
   dragging = true;
@@ -51,10 +59,13 @@ function stopDrag(m) {
 function zoomInStep() {
   img.style.width = `${img.clientWidth * 2}px`;
   img.style.height = `${img.clientHeight * 2}px`;
+  reCenter();
 }
 function zoomOutStep() {
   img.style.width = `${img.clientWidth / 2}px`;
   img.style.height = `${img.clientHeight / 2}px`;
+  //  img.style.left = `${window.innerWidth / 2 - img.clientWidth / 4}px`;
+  reCenter();
 }
 function reCenter() {
   //current size
