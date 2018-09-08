09b37ec4121988dbaf7a49c07657575299fcd580
liyan <yanx0167@stud.kea.dk>
2018-09-08 08:11:31 +0200

    styling of index page

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 "use strict";
+const liS = document.querySelectorAll("li");
+const iframeS = document.querySelectorAll("iframe");
+let iframe;
+liS.forEach((li, index) => checkIfDone(li, index));
+function checkIfDone(li, index) {
+  if (index < iframeS.length) iframe = iframeS[index];
+
+  if (!iframe.getAttribute("src")) liS[index].style.color = "lightgrey";
+}
