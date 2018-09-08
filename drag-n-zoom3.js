59a79e077e1b5b7fad24c187a3956b5fdbd31157
liyan <yanx0167@stud.kea.dk>
2018-09-08 19:11:54 +0200

    re-center finished

---
+++ drag-n-zoom3.js
@@ -0,0 +1,63 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded", init);
+let mousePositionX;
+let mousePositionY;
+let mouseStartPositionX;
+let mouseStartPositionY;
+let mouseStopPositionX;
+let mouseStopPositionY;
+let previousPositionX = 0;
+let previousPositionY = 0;
+let dragging = false;
+const img = document.querySelector(".img");
+const zoomIn = document.querySelector(".zoom-in");
+const zoomOut = document.querySelector(".zoom-out");
+const center = document.querySelector(".center");
+
+function init() {
+  window.addEventListener("mousedown", startDrag);
+  window.addEventListener("mousemove", whileDrag);
+  window.addEventListener("mouseup", stopDrag);
+  zoomIn.addEventListener("click", zoomInStep);
+  zoomOut.addEventListener("click", zoomOutStep);
+  center.addEventListener("click", reCenter);
+}
+function startDrag(m) {
+  dragging = true;
+  mouseStartPositionX = m.pageX;
+  mouseStartPositionY = m.pageY;
+}
+function whileDrag(m) {
+  if (dragging === true) {
+    mousePositionX = m.pageX;
+    mousePositionY = m.pageY;
+    img.style.left = `${-previousPositionX +
+      mousePositionX -
+      mouseStartPositionX}px`;
+    img.style.top = `${-previousPositionY +
+      mousePositionY -
+      mouseStartPositionY}px`;
+  }
+}
+function stopDrag(m) {
+  mouseStopPositionX = m.pageX;
+  mouseStopPositionY = m.pageY;
+  previousPositionX += mouseStartPositionX - mouseStopPositionX;
+  previousPositionY += mouseStartPositionY - mouseStopPositionY;
+  dragging = false;
+}
+
+function zoomInStep() {
+  img.style.width = `${img.clientWidth * 2}px`;
+  img.style.height = `${img.clientHeight * 2}px`;
+}
+function zoomOutStep() {
+  img.style.width = `${img.clientWidth / 2}px`;
+  img.style.height = `${img.clientHeight / 2}px`;
+}
+function reCenter() {
+  //current size
+  img.style.left = `${(window.innerWidth - img.clientWidth) / 2}px`;
+  img.style.top = `${(window.innerHeight - img.clientHeight) / 2}px`;
+}
