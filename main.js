0972ca45dc132f5c7fd8def2094b6b612fc4d836
liyan <yanx0167@stud.kea.dk>
2018-09-08 09:31:38 +0200

    added function when click on li jump to corresponding section. NEW: with scrollIntoView. some  styling change

--- main.js
+++ main.js
@@ -2,9 +2,31 @@
 const liS = document.querySelectorAll("li");
 const iframeS = document.querySelectorAll("iframe");
 let iframe;
-liS.forEach((li, index) => checkIfDone(li, index));
+
+window.addEventListener("DOMContentLoaded", init);
+
+function init() {
+  liS.forEach((li, index) => checkIfDone(li, index));
+  //  liS.forEach((li, index) => li.addEventListener("click", console.log(index)));
+}
+
 function checkIfDone(li, index) {
   if (index < iframeS.length) iframe = iframeS[index];
 
-  if (!iframe.getAttribute("src")) liS[index].style.color = "lightgrey";
+  if (!iframe.getAttribute("src")) {
+    liS[index].style.color = "lightgrey";
+    liS[index].style.cursor = "none";
+    iframe.parentElement.querySelector("h1").style.color = "lightgrey";
+    iframe.style.height = "0";
+  }
+  // instead of using the commented out code to seperate the function (which files without click event), use the code below
+  li.addEventListener("click", jumpToSection);
+  function jumpToSection() {
+    console.log(iframeS[index].offsetTop);
+    iframeS[index].parentElement.scrollIntoView({
+      block: "start",
+      inline: "nearest",
+      behavior: "smooth"
+    });
+  }
 }
