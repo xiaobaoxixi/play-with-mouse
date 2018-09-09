384b237ae39e314b4dc8048766dcf5ad504187ab
liyan <yanx0167@stud.kea.dk>
2018-09-09 17:32:28 +0200

    mouse drag finished

--- touch-n-click.js
+++ touch-n-click.js
@@ -6,8 +6,8 @@ let mouseStartPositionX;
 let mouseStartPositionY;
 let mouseStopPositionX;
 let mouseStopPositionY;
-let previousPositionX = 0;
-let previousPositionY = 0;
+let currentPositionX;
+let currentPositionY;
 let dragging = false;
 
 const allDivS = document.querySelectorAll("body>div>div>div[class]");
@@ -17,38 +17,39 @@ function init() {
   allDivS.forEach(listen);
   function listen(d) {
     d.addEventListener("mousedown", startDrag);
+    d.addEventListener("touchstart", startTouch);
+    d.addEventListener("touchmove", whileTouch);
+    document.addEventListener("touchend", stopTouch);
+    document.addEventListener("touchcancel", stopTouch);
   }
 }
 
 function startDrag(m) {
-  console.log(m.target);
+  m.target.addEventListener("mousemove", whileDrag);
+  document.addEventListener("mouseup", stopDrag);
+
+  mouseStartPositionX = m.pageX;
+  mouseStartPositionY = m.pageY;
+  currentPositionX = m.target.offsetLeft;
+  currentPositionY = m.target.offsetTop;
 }
-// function init() {
-//   big.addEventListener("mousedown", startDrag);
-//   big.addEventListener("mousemove", whileDrag);
-//   big.addEventListener("mouseup", stopDrag);
-// }
-// function startDrag(m) {
-//   dragging = true;
-//   mouseStartPositionX = m.pageX;
-//   mouseStartPositionY = m.pageY;
-// }
-// function whileDrag(m) {
-//   if (dragging === true) {
-//     mousePositionX = m.pageX;
-//     mousePositionY = m.pageY;
-//     big.style.left = `${-previousPositionX +
-//       mousePositionX -
-//       mouseStartPositionX}px`;
-//     big.style.top = `${-previousPositionY +
-//       mousePositionY -
-//       mouseStartPositionY}px`;
-//   }
-// }
-// function stopDrag(m) {
-//   mouseStopPositionX = m.pageX;
-//   mouseStopPositionY = m.pageY;
-//   previousPositionX += mouseStartPositionX - mouseStopPositionX;
-//   previousPositionY += mouseStartPositionY - mouseStopPositionY;
-//   dragging = false;
-// }
+function whileDrag(m) {
+  m.target.style.left = `${currentPositionX + m.pageX - mouseStartPositionX}px`;
+  m.target.style.top = `${currentPositionY + m.pageY - mouseStartPositionY}px`;
+}
+function stopDrag(m) {
+  m.target.removeEventListener("mousemove", whileDrag);
+}
+
+function startTouch(m) {
+  alert("touch start");
+}
+function startTouch(m) {
+  alert("touch start");
+}
+function whileTouch(m) {
+  alert("touching");
+}
+function stopTouch(m) {
+  alert("touch stop");
+}
