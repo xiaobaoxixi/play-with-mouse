25cba7f62327ca1cdf042d569bbd11aeba0c41ed
liyan <yanx0167@stud.kea.dk>
2018-09-15 20:04:41 +0200

    finished. css changes important, including scroll detection on window

--- overlap.js
+++ overlap.js
@@ -4,17 +4,28 @@ window.addEventListener("DOMContentLoaded", init);
 
 function init() {
   let bg = document.querySelector(".bg");
-  let borderRight = bg.getBoundingClientRect().left + bg.clientWidth;
-  let borderTop = bg.getBoundingClientRect().top;
-  console.log(borderTop);
+  //   let borderRight = bg.getBoundingClientRect().left + bg.clientWidth;
+  //   let borderTop = bg.getBoundingClientRect().top;
+  let p = document.querySelector(".text p").textContent.split("");
+  let text = document.querySelector(".text");
+  text.querySelector("p").innerHTML = "";
+  p.forEach(letter => {
+    text.querySelector("p").innerHTML += `<span>${letter}</span>`;
+  });
   let spanS = document.querySelectorAll("span");
+  let radius = window.innerWidth * 0.5;
   spanS.forEach(checkIfOverlap);
 
+  window.addEventListener("scroll", function() {
+    spanS.forEach(checkIfOverlap);
+  }); // if body has fixed height to 100vh, window event "scroll" won't be triggered
   function checkIfOverlap(s) {
     let x = s.getBoundingClientRect().left; // offsetLeft is offset to the parent element, getBCR is to window
     let y = s.getBoundingClientRect().top;
-    console.log(y);
-    if (x < borderRight && y > borderTop) {
+    let borderY =
+      window.innerHeight - Math.sqrt(Math.pow(radius, 2) - Math.pow(x, 2));
+    if (x < radius - 48 && y > borderY + 16) {
+      // =/+ based on current font size, not accurate
       s.style.color = "white";
     } else {
       s.style.color = "orange";
